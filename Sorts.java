6b75f182e5d082a710cc42801a0085b73d0bfb77
moonlee7777 <moonlee7777@gmail.com>
2018-12-05 10:34:08 -0500

    Added selectionsort

--- Sorts.java
+++ Sorts.java
@@ -1,3 +1,17 @@
 public class Sorts {
-  
+  public static void selectionsort(int [] ary) { 
+    int smallest = ary[0];
+    for (int y = 0; y < ary.length; y++) {
+      for (int i = 0; i < ary.length; i++) {
+        if (ary[i] < ary[smallest]) {
+          smallest = i;
+        }
+      }
+      int temp = ary[y];
+      ary[y] = ary[smallest];
+      ary[smallest] = temp;
+    }
+    return ary;
+  }
+
 }

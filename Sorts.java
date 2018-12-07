ac6d102bcf5895113723eec8a0c3f385a69ae3e0
Yongho Lee <ylee01@stuy.edu>
2018-12-07 11:45:47 -0500

    Fixed bugs

--- Sorts.java
+++ Sorts.java
@@ -1,8 +1,26 @@
+import java.util.Random;
+import java.util.Arrays;
 public class Sorts {
-  public static void selectionsort(int [] ary) { 
-    int smallest = ary[0];
+
+  public static void main(String[] args) {
+    int counter = 0;
+    int[] ary = new int[30];
+    Random ran = new Random();
+    while (counter < 30) {
+      ary[counter] = Math.abs(ran.nextInt() % 300);
+      counter++;
+    }
+    System.out.println(Arrays.toString(selectionsort(ary)));
+  }
+
+  public static void bubbleSort(int[] data) {
+  }
+
+  public static int[] selectionsort(int [] ary) {
+    int smallest;
     for (int y = 0; y < ary.length; y++) {
-      for (int i = 0; i < ary.length; i++) {
+      smallest = y;
+      for (int i = y; i < ary.length; i++) {
         if (ary[i] < ary[smallest]) {
           smallest = i;
         }

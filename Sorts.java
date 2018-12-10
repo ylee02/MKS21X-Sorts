c8ac7c534ad16fe831e123915e6ea896b765cdc8
Yongho Lee <ylee01@stuy.edu>
2018-12-10 11:22:35 -0500

    Fixed typos

--- Sorts.java
+++ Sorts.java
@@ -15,11 +15,11 @@ public class Sorts {
 
   public static int[] bubbleSort(int[] data) {
     for (int i = 0; i < data.length - 1; i++) {
-      for (int j = 0; j < data.lenth - i - 1; j++) {
-        if (data[j] > data[j+1]) {
+      for (int j = 0; j < data.length - i - 1; j++) {
+        if (data[j] > data[j + 1]) {
           int temp = data[j];
-          data[j] = data[j+1];
-          data[j+1] = temp;
+          data[j] = data[j + 1];
+          data[j + 1] = temp;
         }
       }
     }

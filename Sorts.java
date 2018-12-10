8020e153d5fd44ed2c31e5ed5057cc8cc5ed7c2d
Yongho Lee <ylee01@stuy.edu>
2018-12-10 11:17:31 -0500

    Quick bug fixes

--- Sorts.java
+++ Sorts.java
@@ -4,19 +4,30 @@ public class Sorts {
 
   public static void main(String[] args) {
     int counter = 0;
-    int[] ary = new int[30];
+    int[] ary = new int[300];
     Random ran = new Random();
-    while (counter < 30) {
-      ary[counter] = Math.abs(ran.nextInt() % 300);
+    while (counter < 300) {
+      ary[counter] = ran.nextInt() % 300;
       counter++;
     }
-    System.out.println(Arrays.toString(selectionsort(ary)));
+    System.out.println(Arrays.toString(selectionSort(ary)));
   }
 
-  public static void bubbleSort(int[] data) {
+  public static int[] bubbleSort(int[] data) {
+    int n = data.length;
+    for (int i = 0; i < n-1; i++) {
+      for (int j = 0; j < n-i-1; j++) {
+        if (data[j] > data[j+1]) {
+          int temp = data[j];
+          data[j] = data[j+1];
+          data[j+1] = temp;
+        }
+      }
+    }
+    return data;
   }
 
-  public static int[] selectionsort(int [] ary) {
+  public static int[] selectionSort(int[] ary) {
     int smallest;
     for (int y = 0; y < ary.length; y++) {
       smallest = y;
